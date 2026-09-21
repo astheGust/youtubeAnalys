a3c453b4a179a912f57a970fcf23c5d6b77ff2c4
astheGust <111141912+astheGust@users.noreply.github.com>
2026-09-21 20:58:51 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-client = genai.Client(api_key="AQ.Ab8RN6IaEsQpe6eKZhRktB4YqrrqsqIyHVuHV7IWA4nwPlZ1YA")
-
-print("List of models that support generateContent:\n")
-for m in client.models.list():
-    for action in m.supported_actions:
-        if action == "generateContent":
-            print(m.name)
